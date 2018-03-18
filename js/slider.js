01ce25081a9e587616e88a3d39e636173ac14f24
legendmage <2646499732.com>
2018-03-18 21:40:12 +0800

    产品展示

--- js/slider.js
+++ js/slider.js
@@ -47,13 +47,6 @@ $(function ($) {
 
     })
 
-    // $(".scrollleft").imgscroll({
-    //     speed: 40,    //图片滚动速度
-    //     amount: 0,    //图片滚动过渡时间
-    //     width: 1,     //图片滚动步数
-    //     dir: "left"   // "left" 或 "up" 向左或向上滚动
-    // });
-
 })
 
 
